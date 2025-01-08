609df189811ef679c34aeda94a70fd1b64ec04f3
nhuytran122 <nhuytran12223@gmail.com>
2025-01-08 22:38:41 +0700

    fix bug when dedletee user

--- Modal/BookmarkModal/BookmarkBo.java
+++ Modal/BookmarkModal/BookmarkBo.java
@@ -3,16 +3,20 @@ package BookmarkModal;
 
 public class BookmarkBo {
 	BookmarkDao bDao = new BookmarkDao();
-	
+
 	public int addBookmark(Long documentID, Long userID) throws Exception {
 		return bDao.addBookmark(documentID, userID);
 	}
-	
+
 	public int deleteBookmark(Long docID, Long userID) throws Exception {
 		return bDao.deleteBookmark(docID, userID);
 	}
-	
+
 	public boolean hasUserMarkedDocs(Long userID, Long docID) throws Exception {
 		return bDao.hasUserMarkedDocs(userID, docID);
+	}
+
+	public int deleteBookmarksByUserID(Long userID) throws Exception {
+		return bDao.deleteBookmarksByUserID(userID);
 	}
 }
